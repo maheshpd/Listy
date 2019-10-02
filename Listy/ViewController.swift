8833d29cc99f3bb5185f7daab31dea2570f2385f
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-10-02 22:13:40 +0530

    Initial Commit

---
+++ Listy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Listy
+//
+//  Created by Mahesh Prasad on 02/10/19.
+//  Copyright © 2019 Mahesh Prasad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
