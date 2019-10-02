fd86c3648d26e80ee7704637afc6a842f34d9cef
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-10-02 23:08:21 +0530

    Configuring ImageArray & Populating UITableView

---
+++ Listy/Images+Names.swift
@@ -0,0 +1,20 @@
+//
+//  Images+Names.swift
+//  Listy
+//
+//  Created by Mahesh Prasad on 02/10/19.
+//  Copyright © 2019 Mahesh Prasad. All rights reserved.
+//
+
+import UIKit
+
+var camera = UIImage(named: "6")!
+var city = UIImage(named: "3")!
+var animal = UIImage(named: "5")!
+var flowers = UIImage(named: "4")!
+var stand = UIImage(named: "1")!
+var urban = UIImage(named: "2")!
+
+var imageArray = [camera,city,animal,flowers,stand,urban]
+
+var nameArray = ["CAMERA","CITY","ANIMAL","FLOWERS","STAND","URBAN"]
--- Listy/ListTableViewController.swift
+++ Listy/ListTableViewController.swift
@@ -21,12 +21,18 @@ class ListTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //Number of Cells
+        return imageArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //return a configured cell with image and name.
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ParallaxCell", for: indexPath) as? ParallaxCell else { return UITableViewCell() }
+        cell.configureCell(withImage: imageArray[indexPath.row], andDescription: nameArray[indexPath.row])
+        
+        return cell
     }
 
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
 }
 
